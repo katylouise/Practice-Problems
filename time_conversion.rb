9b0e1aff3b7a50d1fa11f6913d826db59cfbc3ca
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-14 20:35:44 +0100

    Solution to time conversion

---
+++ time_conversion.rb
@@ -0,0 +1,17 @@
+# Write a method that will take in a number of minutes, and returns a
+# string that formats the number into `hours:minutes`.
+#
+# Difficulty: easy.
+
+def time_conversion(minutes)
+  hours = minutes / 60
+  mins = minutes % 60
+  return "#{hours}:#{mins}"
+end
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+puts('time_conversion(15) == "0:15": ' + (time_conversion(15) == '0:15').to_s)
+puts('time_conversion(150) == "2:30": ' + (time_conversion(150) == '2:30').to_s)
+puts('time_conversion(360) == "6:00": ' + (time_conversion(360) == '6:00').to_s)
