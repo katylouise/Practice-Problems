8617dc404f1fc04fba7738320c767e39fe81804f
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-14 20:31:27 +0100

    Solution to longest word without reduce

---
+++ longest_word_without_reduce.rb
@@ -0,0 +1,34 @@
+ #Write a method that takes in a string. Return the longest word in
+# the string. You may assume that the string contains only letters and
+# spaces.
+#
+# You may use the String `split` method to aid you in your quest.
+#
+# Difficulty: easy.
+
+def longest_word(sentence)
+  sentence_arr = sentence.split(" ")
+    longest = sentence_arr[0]
+  (0...sentence_arr.length).each do |i|
+    if sentence_arr[i].length > longest.length
+      longest = sentence_arr[i]
+    end
+  end
+  return longest
+end
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+puts(
+  'longest_word("short longest") == "longest": ' +
+  (longest_word('short longest') == 'longest').to_s
+)
+puts(
+  'longest_word("one") == "one": ' +
+  (longest_word('one') == 'one').to_s
+)
+puts(
+  'longest_word("abc def abcde") == "abcde": ' +
+  (longest_word('abc def abcde') == 'abcde').to_s
+)
