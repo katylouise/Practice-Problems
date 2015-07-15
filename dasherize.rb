b771241df4a51c6bd0b7a666c6089ce9f4787d04
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-15 20:19:21 +0100

    Partial solution - one bug to fix

---
+++ dasherize.rb
@@ -0,0 +1,46 @@
+# Write a method that takes in a number and returns a string, placing
+# a single dash before and after each odd digit. There is one
+# exception: don't start or end the string with a dash.
+#
+# You may wish to use the `%` modulo operation; you can see if a number
+# is even if it has zero remainder when divided by two.
+#
+# Difficulty: medium.
+
+def dasherize_number(num)
+  num_string = ""
+  num.to_s.split("").each do |n|
+    if n.to_i % 2 == 1
+        num_string << "-#{n}-"
+    else
+      num_string << "#{n}"
+    end
+  end
+  if num_string.start_with?("-")
+       puts num_string.slice!(0)
+  end
+  if num_string.end_with?("-")
+      puts num_string.chop!
+  end
+    puts num_string
+end
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+puts(
+  'dasherize_number(203) == "20-3": ' +
+  (dasherize_number(203) == '20-3').to_s
+)
+puts(
+  'dasherize_number(303) == "3-0-3": ' +
+  (dasherize_number(303) == '3-0-3').to_s
+)
+puts(
+  'dasherize_number(333) == "3-3-3": ' +
+  (dasherize_number(333) == '3-3-3').to_s
+)
+puts(
+  'dasherize_number(3223) == "3-22-3": ' +
+  (dasherize_number(3223) == '3-22-3').to_s
+)
