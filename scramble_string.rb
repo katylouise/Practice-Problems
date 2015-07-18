4be00f7e8b79515b0463c6c54b5abfb42a224859
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-18 18:49:41 +0100

    Solution to scramble string

---
+++ scramble_string.rb
@@ -0,0 +1,25 @@
+# Write a method that takes in a string and an array of indices in the
+# string. Produce a new string, which contains letters from the input
+# string in the order specified by the indices of the array of indices.
+#
+# Difficulty: medium.
+
+def scramble_string(string, positions)
+  new_str = ""
+  positions.each do |n|
+    new_str += string[n]
+  end
+  return new_str
+end
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+puts(
+  'scramble_string("abcd", [3, 1, 2, 0]) == "dbca": ' +
+  (scramble_string("abcd", [3, 1, 2, 0]) == "dbca").to_s
+)
+puts(
+  'scramble_string("markov", [5, 3, 1, 4, 2, 0]) == "vkaorm"): ' +
+  (scramble_string("markov", [5, 3, 1, 4, 2, 0]) == "vkaorm").to_s
+)
